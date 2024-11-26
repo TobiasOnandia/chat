16de3cb67a2f408df59849149317cd73b3ab523d
TobiasOnandia <tobiasonandia0@gmail.com>
2024-11-26 16:38:41 -0300

    intentando mejorar el diseno

--- public/js/script.js
+++ public/js/script.js
@@ -41,79 +41,26 @@ socket.on('chat message', (message, serverOffset, from) => {
     socket.auth.serverOffset = serverOffset
 })
 
+const header = $('header')
 
-const searchInput = $('#search');
-const userList = $('#users');
+header.innerHTML = `
+        <h1>Chat Online</h1>
+        <button class="add">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path d="M0 0h24v24H0z" stroke="none"/><path d="M12 5v14M5 12h14"/></svg>
+        </button>
+`
 
-searchInput.addEventListener('input', (e) => {
-    const filter = e.target.value.toLowerCase();
-    const users = userList.querySelectorAll('li');
-
-    users.forEach(user => {
-        const userName = user.querySelector('h3').textContent.toLowerCase();
-        const userMessage = user.querySelector('p').textContent.toLowerCase();
-
-        if (userName.includes(filter) || userMessage.includes(filter)) {
-            user.style.display = 'block';
-        } else {
-            user.style.display = 'none';
-        }
-    });
-});
+$('.formUser').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const phone = formData.get('phone')
 
 
-const searchMessage = $('#searchMessages');
-const messagesList = $('#messages');
-
-searchMessage.addEventListener('input', (e) => {
-    const filter = e.target.value.toLowerCase();
-    const messages = messagesList.querySelectorAll('li');
-
-    messages.forEach(message => {
-        const messageText = message.querySelector('span').textContent.toLowerCase();
-
-        if (messageText.includes(filter)) {
-            message.style.display = 'flex';
-        } else {
-            message.style.display = 'none';
-        }
-    });
-});
-
-const openDialog = $('.openDialog');
-const closeDialog = $('.closeDialog');
-const dialog = $('.dialog');
-
-openDialog.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-closeDialog.addEventListener('click', () => {
-    dialog.close();
-});
-
-
-const formUser = $('.formUser');
-const users = $('#users');
-
-formUser.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-
-    const user = formData.get('user');
-    const phoneNumber = formData.get('phoneNumber');
-    const email = formData.get('email');
-    
-    if(user && phoneNumber && email){
-        const userElement = document.createElement('li');
-        userElement.innerHTML = `
-             <h3>${user}</h3>
-             <p></p>
-        `;
-
-        users.appendChild(userElement);
-
-    }   
-
-});
+    header.innerHTML = `
+        <h1>+54${phone}</h1>
+        <button class="add">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path d="M0 0h24v24H0z" stroke="none"/><path d="M12 5v14M5 12h14"/></svg>
+        </button>
+`
+})
